c4fd07d9dcf77ce6da352f64b63ac40b2bfa7808
Sem Thijssen <semthijssen@MacBook-Pro-van-Sem.local>
2021-02-03 13:18:50 +0100

    opdrachten af

---
+++ F1M2BO-Quiz-Javascript-main/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI": "https://img.leidschdagblad.nl/ejN0w4arUQRHs23tjlg3gU5IZSM=/fit-in/660x440/https%3A%2F%2Fcdn-kiosk-api.telegraaf.nl%2F0d204f08-39b1-11e9-9edf-9b42e225e743.jpg"
+    },
+    "quizContent":		
+		[
+			{
+				"question": "In welke provincie ligt het LAM?",
+				"answers": [{
+						"answer": "Zuid-holland",
+						"feedback": true
+					},
+					{
+						"answer": "Noord-holland",
+						"feedback": false
+					},
+					{
+						"answer": "Groningen",
+						"feedback": false
+					},
+					{
+						"answer": "Friesland",
+						"feedback": false
+					},
+					{
+						"answer": "Brabant",
+						"feedback": false
+					}
+				]
+				
+			},
+			{
+				"question": "LAM – wanneer is museum LAM opgericht?",
+				"answers": [{
+						"answer": "1974",
+						"feedback": false
+					},
+					{
+						"answer": "1996",
+						"feedback": false
+					},
+					{
+						"answer": "2019",
+						"feedback": true
+					},
+					{
+						"answer": "2010",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "waar hebben alle kunstwerken mee te maken ?",
+				"answers": [{
+						"answer": "geschiedenis",
+						"feedback": false
+					},
+					{
+						"answer": "aarderijkskunde",
+						"feedback": false
+					},
+					{
+						"answer": "het menselijk lichaam",
+						"feedback": false
+					},
+					{
+						"answer": "voedsel",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
---
+++ F1M2BO-Quiz-Javascript-main/quiz2.js
@@ -0,0 +1,65 @@
+const quiz2 = 
+{
+    "quizID": "Math 1",
+    "quizMetaData":
+    {
+        "title": "Math Trivia  1",
+        "imageURI": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dearchitect.nl%2Fprojecten%2Farc18-lisser-art-museum-lam-kvdk-architecten&psig=AOvVaw0PqcWl8UCyibPDrA9Qj22E&ust=1608282472342000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJD33Z3V1O0CFQAAAAAdAAAAABAD"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "What is the next prime number after 7  ",
+				"answers": [{
+						"answer": "9",
+						"feedback": false
+					},
+					{
+						"answer": "10",
+						"feedback": false
+					},
+					{
+						"answer": "11",
+						"feedback": true
+					},
+					{
+						"answer": "13",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "65 - 43 =  ?",
+				"answers": [{
+						"answer": "12",
+						"feedback": false
+					},
+					{
+						"answer": "22",
+						"feedback": true
+					},
+					{
+						"answer": "32",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Are the opposite angles of a parallelogram equal?",
+				"answers": [{
+						"answer": "no",
+						"feedback": false
+					},
+					{
+						"answer": "sometimes",
+						"feedback": false
+					},
+					{
+						"answer": "yes",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
